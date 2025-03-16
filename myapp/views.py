1e6f81add4317770bcd9dbafc5a68bc83ac9f28c
Luis Delgado <luisgdelgadoc@gmail.com>
2025-03-16 14:51:08 -0300

    profin abrio index ahora el herror en articulos

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Articulo
 
 def index(request):
-    return render(request, 'index.html')  
+    return render(request, 'myapp/index.html')
 def lista_articulos(request):
     articulos = Articulo.objects.all()
-    return render(request, 'lista_articulos.html', {'articulos': articulos})  
+    return render(request, 'myapp/lista_articulos.html', {'articulos': articulos})  
