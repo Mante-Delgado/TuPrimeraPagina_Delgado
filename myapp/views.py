ecc1b3b5daad4fc6ac5fce05e9bf51b467f924d2
Luis Delgado <luisgdelgadoc@gmail.com>
2025-03-16 12:21:44 -0300

    Configuración de vistas y URLs para la página de inicio y listado de artículos

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Articulo
 
-# Create your views here.
+def index(request):
+    return render(request, 'myapp/index.html')
+
+def lista_articulos(request):
+    articulos = Articulo.objects.all().order_by('-fecha_publicacion')
+    return render(request, 'myapp/lista_articulos.html', {'articulos': articulos})
